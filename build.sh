e16ed0bf04a18d74d32116d63770eb164ef4a8f8
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-14 15:52:46 -0700

    removed the security unlock to see what error comes up as there is no default keychain

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -ex
 PROFILE_HOME=~/Library/MobileDevice/Provisioning\ Profiles/
 KEYCHAIN=~/Library/Keychains/login.keychain
 
-security unlock -p `cat ~/.build_password`
+#security unlock -p `cat ~/.build_password`
 
 xcodebuild
