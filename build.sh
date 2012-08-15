4af93f69226ed63bc85c9d12d1dced92b6fefe5e
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 12:04:08 -0700

    specified keychain to use

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-security list-keychains -s /Users/KurtisRatio/Library/Keychains/login.keychain
+security unlock-keychain -p `cat ~/.build_password` /Users/KurtisRatio/Library/Keychains/login.keychain
 
-#security unlock-keychain -p `cat ~/.build_password`
 xcodebuild
 
 echo "Got it Working!!!!"
