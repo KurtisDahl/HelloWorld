3aed1b72289b20662d5027f2461c034334d5d497
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 11:59:57 -0700

    fixed user

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-security list-keychains -s /Users/[user]/Library/Keychains/login.keychain
+security list-keychains -s /Users/KurtisRatio/Library/Keychains/login.keychain
 
 #security unlock-keychain -p `cat ~/.build_password`
 xcodebuild
