fe34877a7ad2f4c74ff0cb3e7b1fe45f8517615c
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 11:57:25 -0700

    changed it to see if you need to set up default keychain

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-security unlock-keychain -p `cat ~/.build_password`
+security list-keychains -s /Users/[user]/Library/Keychains/login.keychain
+
+#security unlock-keychain -p `cat ~/.build_password`
 xcodebuild
 
 echo "Got it Working!!!!"
