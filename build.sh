403f1ba1952451fa61338fdaa7557b3f82a2e687
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 11:52:48 -0700

    removed specific key chain location

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-security unlock-keychain -p `cat ~/.build_password` /Users/Shared/Jenkins/Library/Keychains/jenkins.keychain
-
+security unlock-keychain -p `cat ~/.build_password`
 xcodebuild
 
 echo "Got it Working!!!!"
