95eb736c64dfa262706ebce1adb1d5e47fc46c0d
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 09:36:12 -0700

    taking a new approach

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-function failed()
-{
-    echo "Failed $*: $@" >&2
-    exit 1
-}
+security unlock-keychain -p "Kurtis Dahl" /Users/Jenkins/Keychains/login.keychain
 
-set -ex
-
-PROFILE_HOME=~/Library/MobileDevice/Provisioning\ Profiles/
-KEYCHAIN=~/Library/Keychains/login.keychain
-
-#security unlock -p `cat ~/.build_password`
-
-echo $CONFIGURATIONS
-
-for config in $CONFIGURATIONS; do
-echo "Made it into the configs loop"
-echo xcodebuild -activetarget -configuration $config build > Klogfile.txt || failed build;
-
-
-done
+xcodebuild
