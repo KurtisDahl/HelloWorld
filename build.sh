ed5c5bfef23a11b0b02209de47084b1ea517675d
Kurtis Dahl <kurtis@ratiointeractive.com>
2012-08-15 12:27:53 -0700

    reverted to having to copy certs and keys to jenkins folder

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-security unlock-keychain -p `cat ~/.build_password` "Users/KurtisRatio/Library/Keychains/login.keychain"
-security default-keychain -s "Users/KurtisRatio/Library/Keychains/login.keychain"
+security unlock-keychain -p `cat ~/.build_password`
 
 xcodebuild
 
